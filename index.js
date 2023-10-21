d9026c13593360a2b73b25c939c36a0e23d0a726
bony_musau <boniface.musau@moringaschool.com>
2023-10-21 23:11:22 +0300

    DOMContentLoaded assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const par = document.querySelector('#text')
+par.innerText = 'This is really cool'
+par.addEventListener('DOMContentLoaded',function(){
+    console.log(par)
+})
